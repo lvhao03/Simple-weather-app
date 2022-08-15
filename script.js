ae936984a68a29bf67c75bb1dc8eacb1871e3ed1
lvhao03 <luongvihao03@gmail.com>
2022-08-15 19:00:34 +0700

    change layout

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+/* eslint-disable camelcase */
 const cityName = document.querySelector('.city_name');
 const cityTemperture = document.querySelector('.temperture');
 const cityHumidity = document.querySelector('.humidlity');
 const cityWind = document.querySelector('.wind');
 const weatherName = document.querySelector('.weather_name');
+const feelLike = document.querySelector('.feel_like');
 const img = document.querySelector('.icon');
 
+const input = document.querySelector('.input');
+const submitBtn = document.querySelector('.submit');
 const reader = new FileReader();
+
+function getUserInput(event) {
+  event.preventDefault();
+  return input.value;
+}
 
 reader.onloadend = () => {
   const base64data = reader.result;
@@ -13,20 +22,21 @@ reader.onloadend = () => {
 };
 
 function setData(data) {
-  const { humidity, temp } = data.main;
+  const { humidity, temp, feels_like } = data.main;
   cityName.textContent = data.name;
   cityTemperture.innerText = temp;
   cityHumidity.textContent = humidity;
   cityWind.textContent = data.wind.speed;
   weatherName.textContent = data.weather[0].main;
+  feelLike.textContent = feels_like;
 }
 async function getData() {
-  const respone = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Th%C3%A0nh%20ph%E1%BB%91%20H%E1%BB%93%20Ch%C3%AD%20Minh,VN&units=metric&appid=908e8e4dc8a81d0954cd688cc1dd27cb');
+  const location = getUserInput(event);
+  const respone = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=908e8e4dc8a81d0954cd688cc1dd27cb&lang=vi`);
   const data = await respone.json();
   const respone2 = await fetch(`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
   const icon = await respone2.blob();
   reader.readAsDataURL(icon);
   setData(data);
 }
-
-getData();
+submitBtn.addEventListener('click', getData);
