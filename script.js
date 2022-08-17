02e35f602f53ae21b8e750166d63dca97414b746
lvhao03 <luongvihao03@gmail.com>
2022-08-17 18:47:58 +0700

    refactor some functions

--- script.js
+++ script.js
@@ -55,35 +55,47 @@ function showCurrentDate(data) {
 
 function showNextFewDays(data) {
   const arr = data.list;
-  let j = 8;
+  let groupHoursPerDay = 8;
   for (let i = 0; i < tempMax.length; i++) {
-    const { temp_min, temp_max } = arr[j].main;
-    weatherDate[i].textContent = moment.unix(arr[j].dt).format('DD/MM/YY');
+    const { temp_min, temp_max } = arr[groupHoursPerDay].main;
+    weatherDate[i].textContent = moment.unix(arr[groupHoursPerDay].dt).format('DD/MM/YY');
     tempMax[i].textContent = temp_max + '°';
     tempMin[i].textContent = temp_min + '°';
-    j += 8;
+    groupHoursPerDay += 8;
   }
 }
 
-async function setImages(data) {
+async function getImages(data) {
   const arr = data.list;
   let j = 0;
   for (let i = 0; i < mutipleImages.length; i++) {
     const respone = await fetch(`https://openweathermap.org/img/wn/${arr[j].weather[0].icon}@2x.png`);
     const imageBlob = await respone.blob();
     reader.readAsDataURL(imageBlob);
-    j += 9;
+    j += 8;
   }
 }
 
-async function getData() {
+function fetchGeoLocation() {
   const location = getUserInput(event);
-  const respone3 = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=908e8e4dc8a81d0954cd688cc1dd27cb`);
-  const a = await respone3.json();
-  const { lat, lon } = a[0];
-  const respone = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=908e8e4dc8a81d0954cd688cc1dd27cb&lang=vi`);
-  const data = await respone.json();
-  setImages(data);
+  return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=908e8e4dc8a81d0954cd688cc1dd27cb`)
+    .then(respone => respone.json());
+}
+
+async function getGeoLocation() {
+  const arr = await fetchGeoLocation();
+  return arr[0];
+}
+
+function getWeatherData(lat, lon) {
+  return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=908e8e4dc8a81d0954cd688cc1dd27cb&lang=vi`)
+      .then(response => response.json());
+}
+
+async function getData() {
+  const { lat, lon } = await getGeoLocation();
+  const data = await getWeatherData(lat, lon);
+  getImages(data);
   showCurrentDate(data);
   showNextFewDays(data);
 }
