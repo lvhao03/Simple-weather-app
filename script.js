5a681b7820bb4c7e03543dfba4f19d22dd577511
lvhao03 <luongvihao03@gmail.com>
2022-08-18 07:43:47 +0700

    add air polution index

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable default-case */
 /* eslint-disable arrow-parens */
 /* eslint-disable no-undef */
 /* eslint-disable prefer-template */
@@ -14,6 +17,7 @@ const cityWind = document.querySelector('.wind');
 const weatherName = document.querySelector('.weather_name');
 const feelLike = document.querySelector('.feel_like');
 const mutipleImages = document.querySelectorAll('.icon');
+const airPolution = document.querySelector('.air_polution');
 const input = document.querySelector('.input');
 const submitBtn = document.querySelector('.submit');
 const reader = new FileReader();
@@ -41,7 +45,8 @@ function disableHide() {
   });
 }
 
-function showCurrentDate(data) {
+function showCurrentDate(data, airPolutionData) {
+  checkAirQuality(airPolutionData.list[0].main.aqi);
   disableHide();
   const arr = data.list;
   const { humidity, temp, feels_like } = arr[0].main;
@@ -65,6 +70,32 @@ function showNextFewDays(data) {
   }
 }
 
+function checkAirQuality(index) {
+  const arr = ['#79BC6A', '#99CC00', '#FFCC00', '#FF9900', '#CC0033'];
+  switch (index) {
+    case 1:
+        airPolution.textContent = 'Good';
+        airPolution.style.backgroundColor = arr[0];
+        break;
+    case 2:
+        airPolution.textContent = 'Fair';
+        airPolution.style.backgroundColor = arr[1];
+        break;
+    case 3:
+        airPolution.textContent = 'Moderate';
+        airPolution.style.backgroundColor = arr[2];
+        break;
+    case 4:
+        airPolution.textContent = 'Poor';
+        airPolution.style.backgroundColor = arr[3];
+        break;
+    case 5:
+        airPolution.textContent = 'Very Poor';
+        airPolution.style.backgroundColor = arr[4];
+        break;
+  }
+}
+
 async function getImages(data) {
   const arr = data.list;
   let j = 0;
@@ -74,6 +105,7 @@ async function getImages(data) {
     reader.readAsDataURL(imageBlob);
     j += 8;
   }
+  numberOfImage = 0;
 }
 
 function fetchGeoLocation() {
@@ -82,21 +114,27 @@ function fetchGeoLocation() {
     .then(respone => respone.json());
 }
 
+function getWeatherData(lat, lon) {
+  return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=908e8e4dc8a81d0954cd688cc1dd27cb&lang=vi`)
+  .then(response => response.json());
+}
+
+function getAirPolutionData(lat, lon) {
+  return fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=908e8e4dc8a81d0954cd688cc1dd27cb&lang=vi`)
+  .then(respone => respone.json());
+}
+
 async function getGeoLocation() {
   const arr = await fetchGeoLocation();
   return arr[0];
 }
 
-function getWeatherData(lat, lon) {
-  return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=908e8e4dc8a81d0954cd688cc1dd27cb&lang=vi`)
-      .then(response => response.json());
-}
-
 async function getData() {
   const { lat, lon } = await getGeoLocation();
-  const data = await getWeatherData(lat, lon);
-  getImages(data);
-  showCurrentDate(data);
-  showNextFewDays(data);
+  const weatherData = await getWeatherData(lat, lon);
+  const airPolutionData = await getAirPolutionData(lat, lon);
+  getImages(weatherData);
+  showCurrentDate(weatherData, airPolutionData);
+  showNextFewDays(weatherData);
 }
 submitBtn.addEventListener('click', getData);
